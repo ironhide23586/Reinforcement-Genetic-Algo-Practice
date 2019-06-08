a3865a648584caff6190baea4ac6ebf26c1cac4d
Souham Biswas <souham.biswas@here.com>
2019-06-08 17:56:54 +0530

    added initial generator.py for asynchronous data read operation

---
+++ numpy_neural/generator.py
@@ -0,0 +1,171 @@
+import itertools
+import logging
+from threading import Thread
+from queue import Queue
+import time
+import os
+
+import numpy as np
+import cv2
+
+
+class TrainFeeder:
+
+    def __init__(self, data_dir, xy_fpaths, shuffle=True, batch_size=8,
+                 batches_per_queue=40, smart_resize=False,
+                 smart_squareify_side=720):
+        self.data_dir = data_dir
+        fpath_sets = np.array([xy_fpath.strip().split(' ') for xy_fpath in xy_fpaths])
+        self.train_fpaths = []
+        for fpath_set in fpath_sets:
+            x_path, y_path = [os.sep.join([self.data_dir, fpath]) for fpath in fpath_set]
+            self.train_fpaths.append(' '.join([x_path, y_path]))
+        self.train_fpaths = np.array(self.train_fpaths)
+
+        self.smart_resize = smart_resize
+        self.smart_squareify_side = smart_squareify_side
+        logging.info('Initializing TrainFeeder Object on ' + str(self.train_fpaths.shape[0]) + ' data objects')
+        if shuffle:
+            np.random.shuffle(self.train_fpaths)
+        self.shuffle = shuffle
+        self.batch_size = batch_size
+        self.epochs = 0
+        self.batch_iters = 0
+        self.epoch_size_total = self.train_fpaths.shape[0]
+        if self.batch_size > self.epoch_size_total:
+            logging.warning('Batch size exceeds epoch size, setting batch size to epoch size')
+            self.batch_size = self.epoch_size_total
+        self.batches_per_epoch = self.epoch_size_total // self.batch_size
+        self.epoch_size = self.batch_size * self.batches_per_epoch
+        self.batch_data_x = []
+        self.batch_data_y = []
+        self.batch_data_x_fpaths = []
+        self.batch_data_y_fpaths = []
+        self.batch_fpaths = []
+        self.im_coords = None
+        self.total_iters = 0
+        self.train_state = {'epoch': 1, 'batch': 0, 'total_iters': 0,
+                            'previous_epoch_done': False}
+        self.start_batch_queue_populater(batches_per_queue=batches_per_queue)
+
+    def random_sliding_square_crop(self, im):
+        h, w, _ = im.shape
+        if h == w:
+            return im.copy()
+        min_dim = w
+        max_dim = h
+        if h < w:
+            min_dim = h
+            max_dim = w
+        start_idx = np.random.randint(max_dim - min_dim)
+        end_idx = start_idx + min_dim
+        if h < w:
+            im_cropped = im[:, start_idx:end_idx, :]
+        else:
+            im_cropped = im[start_idx:end_idx, :, :]
+        return im_cropped
+
+    def preprocess_set(self, x, y):
+        x_im = x.astype(np.float32)
+        y_im = np.expand_dims(y[:, :, 0], -1).astype(np.float32)
+        h, w, _ = x.shape
+        combined_im = np.concatenate([x_im, y_im], axis=-1)
+        combined_im_cropped = self.random_sliding_square_crop(combined_im)
+        chance_lr = np.random.uniform()
+        chance_ud = np.random.uniform()
+        if chance_lr >= .5:
+            combined_im_cropped = np.fliplr(combined_im_cropped)
+        if chance_ud >= .5:
+            combined_im_cropped = np.flipud(combined_im_cropped)
+        x_im_cropped = combined_im_cropped[:, :, :3]
+        x_im_cropped = cv2.resize(x_im_cropped, (self.smart_squareify_side, self.smart_squareify_side),
+                                  interpolation=cv2.INTER_LANCZOS4)
+        y_im_cropped = combined_im_cropped[:, :, -1]
+        y_im_cropped = cv2.resize(y_im_cropped, (self.smart_squareify_side, self.smart_squareify_side),
+                                  interpolation=cv2.INTER_NEAREST)
+        y_im_cropped = np.expand_dims(y_im_cropped.astype(np.uint8), -1)
+        return x_im_cropped, y_im_cropped
+
+    def fpath2data(self, batch_fpaths):
+        batch_data_x = []
+        batch_data_y = []
+        batch_data_x_fpaths = []
+        batch_data_y_fpaths = []
+        for fpath_set in self.batch_fpaths:
+            x_path, y_path = fpath_set.split(' ')
+            x_im = cv2.imread(x_path)
+            y_im = cv2.imread(y_path)
+            batch_data_x_fpaths.append(x_path)
+            batch_data_y_fpaths.append(y_path)
+            x_im, y_im = self.preprocess_set(x_im, y_im)
+            batch_data_x.append(x_im)
+            batch_data_y.append(y_im)
+        batch_data_x = np.array(batch_data_x)
+        batch_data_y = np.array(batch_data_y)
+        batch_data_x_fpaths = np.array(batch_data_x_fpaths)
+        batch_data_y_fpaths = np.array(batch_data_y_fpaths)
+        return batch_data_x, batch_data_y, batch_data_x_fpaths, batch_data_y_fpaths
+
+    def get_data(self, batch_size=None):
+        if batch_size is not None:
+            logging.info('External batch_size provided, recomputing batches_per_epoch and epoch_size')
+            self.batch_size = batch_size
+            if self.batch_size > self.epoch_size_total:
+                logging.warning('Batch size exceeds epoch size, setting batch size to epoch size')
+                self.batch_size = self.epoch_size_total
+            self.batches_per_epoch = self.epoch_size_total // self.batch_size
+            self.epoch_size = self.batch_size * self.batches_per_epoch
+        self.batch_iters += 1
+        self.total_iters += 1
+        self.epoch_completed = False
+        if self.batch_iters > self.batches_per_epoch:
+            logging.info('---------> Loading new epoch in to Buffered Batch Reader')
+            self.epoch_completed = True
+            self.batch_iters = 1
+            self.epochs += 1
+            if self.shuffle:
+                logging.info('Shuffle enabled, so shuffling input at epoch-level')
+                np.random.shuffle(self.train_fpaths)
+        train_state = {'epoch': self.epochs + 1, 'batch': self.batch_iters, 'total_iters': self.total_iters,
+                       'previous_epoch_done': self.epoch_completed}
+        start_idx = (self.batch_iters - 1) * self.batch_size
+        end_idx = start_idx + self.batch_size
+        self.batch_fpaths = self.train_fpaths[start_idx:end_idx]
+        try:
+            batch_data_x, batch_data_y, batch_data_x_fpaths, batch_data_y_fpaths = self.fpath2data(self.batch_fpaths)
+        except:
+            print(self.batch_fpaths)
+        return batch_data_x, batch_data_y, batch_data_x_fpaths, batch_data_y_fpaths, train_state
+
+    def __queue_filler_process(self):
+        while True:
+            if self.buffer.full():
+                # logging.info('Buffer full, sleeping for 2 seconds')
+                time.sleep(2)
+                continue
+            try:
+                self.buffer.put(self.get_data())
+            except:
+                logging.error('Buffer Load error!, retyring...')
+                continue
+
+    def start_batch_queue_populater(self, batches_per_queue=20):
+        logging.info('Starting Populator process for batch buffer')
+        self.buffer = Queue(maxsize=batches_per_queue)
+        self.queue_filler_thread = Thread(target=self.__queue_filler_process)
+        self.queue_filler_thread.start()
+
+    def dequeue(self):
+        if not self.buffer.empty():
+            self.batch_data_x, self.batch_data_y, \
+            self.batch_data_x_fpaths, self.batch_data_y_fpaths, self.train_state = self.buffer.get()
+            if self.train_state['previous_epoch_done']:
+                logging.info('----------------EPOCH ' + str(self.train_state['epoch'] - 1)
+                             + ' COMPLETE----------------')
+            logging.info('Epoch ' + str(self.train_state['epoch']) + ', Batch ' + str(self.train_state['batch']))
+            return self.batch_data_x, self.batch_data_y
+        else:
+            logging.warning('Buffer empty, waiting for it to repopulate..')
+            while self.buffer.empty():
+                continue
+            return self.dequeue()
